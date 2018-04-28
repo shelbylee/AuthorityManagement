c9c20c965698f30dab7ca1c0d7e2650e24696caa
shelbylee <lixibin0829@gmail.com>
2018-04-28 17:23:59 +0800

    add md5

--- src/main/java/com/lxb/controller/SysUserController.java
+++ src/main/java/com/lxb/controller/SysUserController.java
@@ -1,7 +1,34 @@
 package com.lxb.controller;
 
+import com.lxb.common.JsonData;
+import com.lxb.param.UserParam;
+import com.lxb.service.SysUserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.annotation.Resource;
+
+@Controller
 @RequestMapping("/sys/user")
+@Slf4j
 public class SysUserController {
+
+    @Resource
+    private SysUserService sysUserService;
+
+    @RequestMapping("/save.json")
+    @ResponseBody
+    public JsonData saveUser(UserParam param) {
+        sysUserService.save(param);
+        return JsonData.success();
+    }
+
+    @RequestMapping("/update.json")
+    @ResponseBody
+    public JsonData updateUser(UserParam param) {
+        sysUserService.update(param);
+        return JsonData.success();
+    }
 }
--- src/main/java/com/lxb/service/SysUserService.java
+++ src/main/java/com/lxb/service/SysUserService.java
@@ -1,10 +1,12 @@
 package com.lxb.service;
 
+import com.google.common.base.Preconditions;
 import com.lxb.dao.SysUserMapper;
 import com.lxb.exception.ParamException;
 import com.lxb.model.SysUser;
 import com.lxb.param.UserParam;
 import com.lxb.util.BeanValidator;
+import com.lxb.util.MD5Util;
 import com.lxb.util.PasswordUtil;
 import org.springframework.stereotype.Service;
 
@@ -27,15 +29,16 @@ public class SysUserService {
             throw new ParamException("该邮箱已被占用");
         }
 
-        String passwd = PasswordUtil.randomPasswd();
+        String password = PasswordUtil.randomPasswd();
         // TODO:
-        passwd = "12345678";
+        password = "12345678";
+        String encryptedPassword = MD5Util.encrypt(password);
 
         SysUser user = SysUser.builder()
                 .username(param.getUsername())
                 .telephone(param.getTelephone())
                 .mail(param.getMail())
-                .password(passwd)
+                .password(encryptedPassword)
                 .deptId(param.getDeptId())
                 .status(param.getStatus())
                 .remark(param.getRemark()).build();
@@ -46,6 +49,30 @@ public class SysUserService {
 
         // TODO: sendEmail
         sysUserMapper.insertSelective(user);
+    }
+
+    public void update(UserParam param) {
+        BeanValidator.check(param);
+        if (checkTelephoneExist(param.getTelephone(), param.getId())) {
+            throw new ParamException("该电话已被占用");
+        }
+        if (checkEmailExist(param.getMail(), param.getId())) {
+            throw new ParamException("该邮箱已被占用");
+        }
+
+        SysUser before = sysUserMapper.selectByPrimaryKey(param.getId());
+        Preconditions.checkNotNull(before, "待更新的用户不存在");
+        SysUser after = SysUser.builder()
+                .id(param.getId())
+                .username(param.getUsername())
+                .telephone(param.getTelephone())
+                .mail(param.getMail())
+                .password(before.getPassword())
+                .deptId(param.getDeptId())
+                .status(param.getStatus())
+                .remark(param.getRemark()).build();
+
+        sysUserMapper.updateByPrimaryKeySelective(after);
     }
 
     public boolean checkEmailExist(String mail, Integer userId) {
