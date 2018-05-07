6c7ea97ba047a0ee2c63749cf85a762788c5c395
shelbylee <lixibin0829@gmail.com>
2018-05-07 11:31:23 +0800

    add changeUsers.json

--- src/main/java/com/lxb/service/SysRoleUserService.java
+++ src/main/java/com/lxb/service/SysRoleUserService.java
@@ -1,14 +1,23 @@
 package com.lxb.service;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+import com.lxb.common.RequestHolder;
+import com.lxb.dao.SysRoleAclMapper;
 import com.lxb.dao.SysRoleUserMapper;
 import com.lxb.dao.SysUserMapper;
+import com.lxb.model.SysRoleAcl;
+import com.lxb.model.SysRoleUser;
 import com.lxb.model.SysUser;
+import com.lxb.util.IpUtil;
 import org.apache.commons.collections.CollectionUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.util.Date;
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class SysRoleUserService {
@@ -29,5 +38,32 @@ public class SysRoleUserService {
         return sysUserMapper.getByIdList(userIdList);
     }
 
+    public void changeRoleUsers(int roleId, List<Integer> userIdList) {
+        List<Integer> originUserIdList = sysRoleUserMapper.getUserIdListByRoleId(roleId);
+        if (originUserIdList.size() == userIdList.size()) {
+            Set<Integer> originUserIdSet = Sets.newHashSet(originUserIdList);
+            Set<Integer> userIdSet = Sets.newHashSet(userIdList);
+            originUserIdSet.removeAll(userIdSet);
+            if (CollectionUtils.isEmpty(originUserIdSet)) {
+                return;
+            }
+        }
+        updateRoleUsers(roleId, userIdList);
+    }
 
+    @Transactional
+    public void updateRoleUsers(int roleId, List<Integer> userIdList) {
+        sysRoleUserMapper.deleteByRoleId(roleId);
+
+        if (CollectionUtils.isEmpty(userIdList)) {
+            return;
+        }
+        List<SysRoleUser> roleUserList = Lists.newArrayList();
+        for (Integer userId : userIdList) {
+            SysRoleUser roleUser = SysRoleUser.builder().roleId(roleId).userId(userId).operator(RequestHolder.getCurrentUser().getUsername())
+                    .operateIp(IpUtil.getRemoteIp(RequestHolder.getCurrentRequest())).operateTime(new Date()).build();
+            roleUserList.add(roleUser);
+        }
+        sysRoleUserMapper.batchInsert(roleUserList);
+    }
 }
